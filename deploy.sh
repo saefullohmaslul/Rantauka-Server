9dd451aac884203a0846bea95db1da180e06a0d7
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2019-08-23 13:50:07 +0700

    adding migrate

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-cd node_modules/.bin/
-sequelize db:migrate:undo
-sequelize db:migrate:undo
-sequelize db:migrate:undo
-sequelize db:migrate:undo
-sequelize db:migrate
+node_modules/.bin/sequelize db:migrate:undo
+node_modules/.bin/sequelize db:migrate:undo
+node_modules/.bin/sequelize db:migrate:undo
+node_modules/.bin/sequelize db:migrate:undo
+node_modules/.bin/sequelize db:migrate
