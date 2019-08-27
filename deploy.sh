ef8fe43e5df4b87feefcaea301ae5bf0b36f62aa
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2019-08-27 15:24:55 +0700

    revision deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-node_modules/.bin/sequelize db:migrate:undo
-node_modules/.bin/sequelize db:migrate:undo
-node_modules/.bin/sequelize db:migrate:undo
-node_modules/.bin/sequelize db:migrate:undo
+# node_modules/.bin/sequelize db:migrate:undo
+# node_modules/.bin/sequelize db:migrate:undo
+# node_modules/.bin/sequelize db:migrate:undo
+# node_modules/.bin/sequelize db:migrate:undo
 node_modules/.bin/sequelize db:migrate
