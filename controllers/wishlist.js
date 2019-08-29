8a5cf44e86f6943bd332f66c07165b11e63947cb
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2019-08-29 12:39:38 +0700

    change error typo

--- controllers/wishlist.js
+++ controllers/wishlist.js
@@ -3,28 +3,28 @@ const Wishlist = require("../models").wishlist;
 const Image = require("../models").image;
 
 exports.index = async (req, res) => {
-    try {
-      const wishlist = await Wishlist.findAll({
-        where: { userId: req.userId },
-        include: [
-          {
-            model: House,
-            include: [
-              {
-                model: Image
-              }
-            ]
-          }
-        ]
-      });
-      res.status(200).send(wishlist);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  try {
+    const wishlist = await Wishlist.findAll({
+      where: { userId: req.userId },
+      include: [
+        {
+          model: House,
+          include: [
+            {
+              model: Image
+            }
+          ]
+        }
+      ]
+    });
+    res.status(200).send(wishlist);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 exports.store = async (req, res) => {
-  const {houseId}  = req.body;
+  const { houseId } = req.body;
 
   try {
     const wishlist = await Wishlist.create({
@@ -40,12 +40,10 @@ exports.store = async (req, res) => {
 };
 
 exports.delete = (req, res) => {
-Wishlist.destroy({where: 
-  {id: req.params.id }})
-  .then(wishlist=> {
-  res.send({
+  Wishlist.destroy({ where: { id: req.params.id } }).then(wishlist => {
+    res.send({
       message: "success",
       wishlist
-  })
-})
-}
+    });
+  });
+};
--- routes/wishlistRoutes.js
+++ routes/wishlistRoutes.js
@@ -3,11 +3,10 @@ require("express-group-routes");
 
 const app = express();
 
-const WishlistCountroller = require("../controllers/Wishlist");
+const WishlistCountroller = require("../controllers/wishlist");
 const isAuth = require("../middlewares/is-auth");
 
 app.group("/api/v1", routes => {
- 
   routes.post("/wishlist", isAuth, WishlistCountroller.store);
   routes.get("/wishlists", isAuth, WishlistCountroller.index);
   routes.delete("/wishlist/:id", isAuth, WishlistCountroller.delete);
